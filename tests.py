4b628b419391cec422d93b0e95f5dcbb7c47181b
WoutHofstra <hofstrawout@gmail.com>
2025-07-14 22:21:53 +0200

    Added write file function

--- tests.py
+++ tests.py
@@ -1,6 +1,17 @@
-from functions.get_files_info import *
+from functions.write_file import write_file
 
-print(get_files_info("calculator", "."))
-print(get_files_info("calculator", "pkg"))
-print(get_files_info("calculator", "/bin"))
-print(get_files_info("calculator", "../"))
+
+def test():
+
+    result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
+    print(result)
+
+    result = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
+    print(result)
+
+    result = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
+    print(result)
+
+
+if __name__ == "__main__":
+    test()
